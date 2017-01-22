3bdd4fe7d4d9f68152a81893bc3344561feb9453
Lei Zhang <leizhang@microsoft.com>
2017-01-21 18:14:58 -0800

    Modify .gitignore to include py-faster-rcnn/lib

---
+++ src/py-faster-rcnn/lib/datasets/tsv_eval.py
@@ -0,0 +1,126 @@
+# --------------------------------------------------------
+# Fast/er R-CNN
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Bharath Hariharan
+# --------------------------------------------------------
+
+import xml.etree.ElementTree as ET
+import os, os.path as op
+import cPickle
+import numpy as np
+import voc_eval
+
+def tsv_eval(gt_roidb, image_index,
+             detpath,
+             classname,
+             ovthresh=0.5,
+             use_07_metric=False):
+    """
+    Top level function that does the PASCAL VOC evaluation.
+    gt_roidb, image_index: passed from tsv data
+    detpath: Path to detections
+        detpath.format(classname) should produce the detection results file.
+    classname: Category name (duh)
+    [ovthresh]: Overlap threshold (default = 0.5)
+    [use_07_metric]: Whether to use VOC07's 11 point AP computation
+        (default False)
+    """
+    # assumes detections are in detpath.format(classname)
+
+    # first load gt
+    recs = {}
+    for i, roidb in enumerate(gt_roidb):
+        objects = []
+        for bbox in roidb['boxes']:
+            obj_struct = {'name': roidb['classname'],
+                               'pose': False,
+                               'truncated': False,
+                               'difficult': False,
+                               'bbox': bbox}
+            objects.append(obj_struct)
+        recs[roidb['imagename']] = objects
+
+    # extract gt objects for this class
+    class_recs = {}
+    npos = 0
+    for line_no in image_index:
+        imagename = str(line_no)
+        R = [obj for obj in recs[imagename] if obj['name'] == classname]
+        bbox = np.array([x['bbox'] for x in R])
+        difficult = np.array([x['difficult'] for x in R]).astype(np.bool)
+        det = [False] * len(R)
+        npos = npos + sum(~difficult)
+        class_recs[imagename] = {'bbox': bbox,
+                                 'difficult': difficult,
+                                 'det': det}
+
+    # read dets
+    detfile = detpath.format(classname)
+    assert op.exists(detfile), 'detfile %s does not exist' % detfile
+    with open(detfile, 'r') as f:
+        lines = f.readlines()
+
+    splitlines = [x.strip().split(' ') for x in lines]
+    image_ids = [x[0] for x in splitlines]
+    confidence = np.array([float(x[1]) for x in splitlines])
+    if len(splitlines) > 0:
+        BB = np.array([[float(z) for z in x[2:]] for x in splitlines])
+    else:
+        BB = np.zeros([0, 4])  # if nothing detected, still need to be two-dim.
+
+    # sort by confidence
+    sorted_ind = np.argsort(-confidence)
+    sorted_scores = np.sort(-confidence)
+    BB = BB[sorted_ind, :]
+    image_ids = [image_ids[x] for x in sorted_ind]
+
+    # go down dets and mark TPs and FPs
+    nd = len(image_ids)
+    tp = np.zeros(nd)
+    fp = np.zeros(nd)
+    for d in range(nd):
+        R = class_recs[image_ids[d]]
+        bb = BB[d, :].astype(float)
+        ovmax = -np.inf
+        BBGT = R['bbox'].astype(float)
+
+        if BBGT.size > 0:
+            # compute overlaps
+            # intersection
+            ixmin = np.maximum(BBGT[:, 0], bb[0])
+            iymin = np.maximum(BBGT[:, 1], bb[1])
+            ixmax = np.minimum(BBGT[:, 2], bb[2])
+            iymax = np.minimum(BBGT[:, 3], bb[3])
+            iw = np.maximum(ixmax - ixmin + 1., 0.)
+            ih = np.maximum(iymax - iymin + 1., 0.)
+            inters = iw * ih
+
+            # union
+            uni = ((bb[2] - bb[0] + 1.) * (bb[3] - bb[1] + 1.) +
+                   (BBGT[:, 2] - BBGT[:, 0] + 1.) *
+                   (BBGT[:, 3] - BBGT[:, 1] + 1.) - inters)
+
+            overlaps = inters / uni
+            ovmax = np.max(overlaps)
+            jmax = np.argmax(overlaps)
+
+        if ovmax > ovthresh:
+            if not R['difficult'][jmax]:
+                if not R['det'][jmax]:
+                    tp[d] = 1.
+                    R['det'][jmax] = 1
+                else:
+                    fp[d] = 1.
+        else:
+            fp[d] = 1.
+
+    # compute precision recall
+    fp = np.cumsum(fp)
+    tp = np.cumsum(tp)
+    rec = tp / float(npos)
+    # avoid divide by zero in case the first detection matches a difficult
+    # ground truth
+    prec = tp / np.maximum(tp + fp, np.finfo(np.float64).eps)
+    ap = voc_eval.voc_ap(rec, prec, use_07_metric)
+
+    return rec, prec, ap
