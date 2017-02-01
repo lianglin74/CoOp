3dc73b89c5a55f6c2c78648418f4d4473fb1bb00
Lei Zhang <leizhang@microsoft.com>
2017-02-01 14:26:35 -0800

    One stop caffe setup

--- README.md
+++ README.md
@@ -13,7 +13,12 @@ This repo is for the algorithm development for IRIS object detection. The curren
    ```
    git clone --recursive https://msresearch.visualstudio.com/DefaultCollection/IRIS/_git/IRISObjectDetection
    ```
-2. Setup `Caffe` by following [Caffe README.md](https://msresearch.visualstudio.com/IRIS/_git/CCSCaffe?path=%2FREADME.md&version=GBWinCaffe&_a=contents)
+2. Setup `Caffe` under the repo root folder:
+   ```
+   python script\setup_caffe.py
+   ```
+   The script will download 3rd party dependencies and build caffe automatically.  
+   See [Caffe README.md](https://msresearch.visualstudio.com/IRIS/_git/CCSCaffe?path=%2FREADME.md&version=GBWinCaffe&_a=contents) for more detailed steps.
 3. Setup py-faster-rcnn by following [py-faster-rcnn README.md](https://msresearch.visualstudio.com/IRIS/_git/IRISObjectDetection?path=%2Fsrc%2Fpy-faster-rcnn%2FREADME.md&version=GBmaster&_a=contents)
 4. Under the repo root folder, copy `Pascal VOC' data:
    ```
---
+++ script/setup_caffe.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import zipfile
+import subprocess
+import multiprocessing
+
+msbuild = r'C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe'
+assert os.path.exists(msbuild), 'Expect MSBuild.exe installed at %s' % msbuild
+cpu_count = multiprocessing.cpu_count()
+
+if not 'PYTHON_ROOT' in os.environ:
+    python_exe = sys.executable
+    assert bool(python_exe), 'python.exe not found'
+    os.environ['PYTHON_ROOT'] = os.path.split(sys.executable)[0]
+    print('set PYTHON_ROOT to %s' % os.environ['PYTHON_ROOT'])
+
+cwd = os.getcwd()
+sys.path.insert(0, os.path.join(cwd, 'src/CCSCaffe/scripts'))
+
+import download_prebuilt_dependencies
+import install_cudnn
+
+os.chdir('src/CCSCaffe')
+
+if not os.path.exists('libraries'):
+    download_prebuilt_dependencies.main([])
+else:
+    print('Skip install prebuilt dependencies. For clean setup, please delete folder <src/CCSCaffe/libraries> and run this script again')
+
+if not os.path.exists('cudnn'):
+    cudnn_zipfile = r'\\ivm-server2\IRIS\IRISObjectDetection\Data\cudnn-8.0-windows10-x64-v5.0-ga.zip'
+    install_cudnn.main([cudnn_zipfile])
+else:
+    print('Skip install cudnn. For clean setup, please delete folder <src/CCSCaffe/cudnn> and run this script again')
+
+print('\n')
+subprocess.call([msbuild, 'caffe.sln', '/t:Build', '/p:Configuration=Release', '/maxcpucount:%d' % max(1, cpu_count - 4)], shell=True)
+
+os.chdir(cwd)
