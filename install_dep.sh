22f471012f019607b4e54667ac8a1a5fc36ce507
megupt <megupt@microsoft.com>
2018-03-21 15:54:30 -0700

    updating dependency

--- install_dep.sh
+++ install_dep.sh
@@ -4,3 +4,6 @@ python -m nltk.downloader all
 # the following is used to install yaml CLoader
 sudo apt-get install libyaml-dev
 sudo pip install pyyaml --upgrade --force
+sudo pip install progressbar
+sudo pip install python-magic
+sudo pip install easydict
