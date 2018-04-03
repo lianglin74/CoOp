1f755051471a669a80e886eeff911bfe62362e41
Jianfeng Wang <jianfengwang@outlook.com>
2018-04-03 09:44:15 -0700

    [compile script] support 16.04 compilation for caffe

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # don't continue if there is any error
 set -e
 
@@ -14,6 +15,12 @@ if [ ! -f 'Makefile.config' ]; then
     echo "USE_NCCL := 1" >> Makefile.config
     # under ubuntu, compilation will fail without c++11 option
     echo "CUSTOM_CXX := g++ --std=c++11" >> Makefile.config
+
+    LSB_RELEASE_INFO=$(lsb_release -r)
+    if [[ $LSB_RELEASE_INFO = *"16.04" ]]; then
+        echo 'INCLUDE_DIRS := $(INCLUDE_DIRS) /usr/include/hdf5/serial' >> Makefile.config
+        echo 'LIBRARY_DIRS := $(LIBRARY_DIRS) /usr/lib/x86_64-linux-gnu/hdf5/serial' >> Makefile.config
+    fi
 fi
 make proto
 make -j
