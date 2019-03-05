01a38882c15c3479331a0865a80943a05c40241f
Jianfeng Wang <jianfengwang@outlook.com>
2019-03-04 18:54:20 -0800

    support utilities to connect mongodb for data labeling

---
+++ src/qd/db.py
@@ -0,0 +1,101 @@
+from .qd_common import load_from_yaml_file
+from pymongo import MongoClient
+import pymongo
+import copy
+
+def create_mongodb_client():
+    config = load_from_yaml_file('./aux_data/configs/mongodb_credential.yaml')
+    host = config['host']
+    return MongoClient(host)
+
+def create_bbverification_db():
+    return BoundingBoxVerificationDB()
+
+class BoundingBoxVerificationDB(object):
+    status_requested = 'requested'
+    status_retrieved = 'retrieved'
+    status_submitted = 'submitted'
+    status_completed = 'completed'
+    status_merged = 'merged'
+    def __init__(self):
+        self.client = None
+        self.db_name = 'qd'
+        self.collection_name = 'uhrs_bounding_box_verification'
+        self.collection = None
+
+    def query_by_pipeline(self, pipeline):
+        self._ensure_client_opened()
+        result = self.collection.aggregate(pipeline, allowDiskUse=True)
+        return list(result)
+
+    def request_by_insert(self, all_box_task):
+        self._ensure_client_opened()
+        all_box_task = copy.deepcopy(all_box_task)
+        for b in all_box_task:
+            assert 'status' not in b
+            b['status'] = self.status_requested
+        self.collection.insert_many(all_box_task)
+
+    def retrieve(self, max_box):
+        assert max_box > 0
+        self._ensure_client_opened()
+        pipeline = [
+                {'$match': {'status': self.status_requested}},
+                {'$sort': {'priority': pymongo.ASCENDING}},
+                {'$limit': max_box},
+                ]
+        result = self.query_by_pipeline(pipeline)
+        # we need to update the status to status_retrieved to avoid duplicate
+        # retrieve && submit
+        self.update_status([r['_id'] for r in result],
+                self.status_retrieved)
+
+        return result
+
+    def update_status(self, all_id, new_status):
+        self.collection.update_many(filter={'_id': {'$in': all_id}},
+                update={'$set': {'status': new_status}})
+
+    def submitted(self, submitted):
+        self.adjust_status(submitted, 'uhrs_submitted_result',
+                'uhrs_submitted_result', self.status_submitted)
+
+    def adjust_status(self, uhrs_results, uhrs_result_field, db_field,
+            new_status):
+        self._ensure_client_opened()
+        for s in uhrs_results:
+            assert uhrs_result_field in s
+            assert '_id' in s
+
+        all_id = [s['_id'] for s in uhrs_results]
+
+        query = {'_id': {'$in': all_id}}
+
+        # save the result from uhrs
+        for s in uhrs_results:
+            self.collection.update_one(filter={'_id': s['_id']},
+                    update={'$set': {db_field: s[uhrs_result_field]}})
+
+        # update the status
+        self.collection.update_many(filter=query,
+                update={'$set': {'status': new_status}})
+
+    def query_submitted(self, topk=None):
+        self._ensure_client_opened()
+        pipeline = [
+                {'$match': {'status': self.status_submitted}},
+                ]
+        if topk:
+            pipeline.append({'$limit': topk})
+        result = self.query_by_pipeline(pipeline)
+        return result
+
+    def complete(self, completed):
+        self.adjust_status(completed, 'uhrs_completed_result',
+                'uhrs_completed_result', self.status_completed)
+
+    def _ensure_client_opened(self):
+        if self.client is None or self.collection is None:
+            self.client = create_mongodb_client()
+            self.collection = self.client[self.db_name][self.collection_name]
+
