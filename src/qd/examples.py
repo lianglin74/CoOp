741c8b461b39ed277080dc21615840e0344a274b
Jianfeng Wang <jianfengwang@outlook.com>
2019-04-01 09:34:01 -0700

    add examples.py to qd
    you can call any function whose name is started with test_ directly to give it a test

---
+++ src/qd/examples.py
@@ -0,0 +1,24 @@
+from .qd_common import init_logging
+
+
+def create_parameters(**kwargs):
+    kwargs.update({'data': 'voc20',
+                   'max_iter': 10000,
+                   'effective_batch_size': 64})
+    return kwargs
+
+def create_pipeline(kwargs):
+    from .qd_pytorch import YoloV2PtPipeline
+    return YoloV2PtPipeline(**kwargs)
+
+def test_model_pipeline(**kwargs):
+    init_logging()
+    kwargs.update(create_parameters(**kwargs))
+    pip = create_pipeline(kwargs)
+
+    if kwargs.get('monitor_train_only'):
+        pip.monitor_train()
+    else:
+        pip.ensure_train()
+        pip.ensure_predict()
+        pip.ensure_evaluate()
