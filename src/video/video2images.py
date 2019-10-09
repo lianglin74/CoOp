4236a60f79f0f832db7c5ad63de0389353bbb7a7
Gavin Wei <yaowe@microsoft.com>
2019-10-09 11:05:29 -0700

    Change to extract images from videos by a file list.

--- src/video/video2images.py
+++ src/video/video2images.py
@@ -1,6 +1,6 @@
 import cv2
 import os
-
+from video.getShotMomentsNew import read_file_to_list
 
 def writeAllFrames(videoFileFullPath, imagePath = None):
     cap = cv2.VideoCapture(videoFileFullPath)
@@ -9,7 +9,8 @@ def writeAllFrames(videoFileFullPath, imagePath = None):
     videoName = os.path.basename(videoFileFullPath)
 
     if imagePath is None:
-        imagePath = dir + "/frames_" + videoName + "/images/"
+        #imagePath = dir + "/frames_" + videoName + "/images/"
+        imagePath = dir + "/frames_" + videoName + "/"
     if not os.path.exists(imagePath):
         os.makedirs(imagePath)
 
@@ -29,7 +30,7 @@ def writeAllFrames(videoFileFullPath, imagePath = None):
             print("Stop getting frame from video: ", videoFileFullPath, ", at frameIndex: ", frameIndex)
             break
         else:
-            imageId  = '%07d' % frameIndex
+            imageId  = videoName.replace('.mp4', '') + '-%02d' % frameIndex
             cv2.imwrite(imagePath + imageId + '.jpg', frame)
 
         frameIndex += 1
@@ -41,7 +42,18 @@ def test_writeAllFrames():
     videoFile = '/mnt/gpu02_raid/data/video/CBA/eventDetection/videosForDunkDetection/train/dunk/NBA2-74_0.mp4'
     writeAllFrames(videoFile)
 
+def videos2Frames_byList():
+    videoFileList = "videoList.txt"
+    #dir = "/mnt/gpu02_raid/data/video/CBA/eventDetection/videosForDunkDetection/train/dunk/"
+    dir = "/mnt/gpu02_raid/data/video/CBA/eventDetection/videosForDunkDetection/train/nonDunk/"
+    fileList = read_file_to_list(dir + videoFileList)
+    fileList = [dir + f for f in fileList]
+
+    for file in fileList:
+        print("--processing ", file)
+        writeAllFrames(file)
 
 if __name__ == "__main__":
-    test_writeAllFrames()
+    #test_writeAllFrames()
+    videos2Frames_byList()
 
