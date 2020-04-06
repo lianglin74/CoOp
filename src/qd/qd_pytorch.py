839b943cb06d8067e3398c3713e1cd85649b45ec
Jianfeng Wang <jianfw@microsoft.com>
2020-04-06 14:54:39 -0700

    add two-crop-transform

--- src/qd/qd_pytorch.py
+++ src/qd/qd_pytorch.py
@@ -258,6 +258,16 @@ class CompositeTSVFile():
         self.tsvs = [TSVFile(f, self.cache_policy) for f in load_list_file(self.list_file)]
         self.initialized = True
 
+class TwoCropsTransform:
+    """Take two random crops of one image as the query and key."""
+
+    def __init__(self, base_transform):
+        self.base_transform = base_transform
+
+    def __call__(self, x):
+        q = self.base_transform(x)
+        k = self.base_transform(x)
+        return [q, k]
 class TSVSplitProperty(Dataset):
     '''
     one instance of this class mean one tsv file or one composite tsv, it could
