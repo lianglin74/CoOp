f0a4ab0e37113eb0998cb601013f96d0ba6587ed
Jianfeng Wang <jianfw@microsoft.com>
2020-08-29 16:35:21 -0700

    update requirements and setup to install dependency automatically

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup
 import setuptools
+import os.path as op
 
 # suggest to install by
 # python setup.py build develop --user
 
+req = '../requirements.txt'
+if op.isfile(req):
+    with open(req) as fp:
+        requirements = fp.readlines()
+else:
+    requirements = []
+
 setup(name='qd',
       version='0.1',
       description='Provide basic utility functions',
-      #packages=['qd', 'qd_classifier'],
       packages=setuptools.find_packages(),
+      install_requires=requirements,
      )
 
