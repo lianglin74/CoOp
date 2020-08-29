b58c662998f9ba0f69e9a8f07f68a0749959c8f2
Jianfeng Wang <jianfw@microsoft.com>
2020-08-29 12:19:08 -0700

    use one setup() in setup.py

--- src/setup.py
+++ src/setup.py
@@ -6,11 +6,6 @@ from setuptools import setup
 setup(name='qd',
       version='0.1',
       description='Provide basic utility functions',
-      packages=['qd'],
+      packages=['qd', 'qd_classifier'],
      )
 
-setup(name='qd_classifier',
-      version='0.1',
-      description='Training code for the 2nd stage classifier after detection',
-      packages=['qd_classifier'],
-     )
